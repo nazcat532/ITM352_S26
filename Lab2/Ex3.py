a1b0b05230165d644c6c73d786d5961d3d921703
nazcat532 <taniguchinazca@gmail.com>
2026-01-22 12:59:30 -1000

    Exercises 3-7

---
+++ Lab2/Ex3.py
@@ -0,0 +1,2 @@
+#Ask the user to enter a floating point number. Square the number
+# Print out the orignal number and the squared result 
---
+++ Lab2/Ex5.py
@@ -0,0 +1,12 @@
+# Ask the user to enter a string of their.
+# Calculate the length of the string and print it out.
+# Name: Nazca
+# Date: 2026-01-22
+
+user_string = input("Please enter a string of your choosing: ")
+string_length = len(user_string)
+
+print("You entered:", user_string)
+print("The length of the string is", string_length)
+
+
